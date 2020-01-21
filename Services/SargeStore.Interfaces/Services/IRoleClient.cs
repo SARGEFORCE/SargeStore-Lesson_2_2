7a0e8d09a016030e1d5b52f3e3715e91415fe103
Sergej Salmin <several989@mail.ru>
2020-01-22 00:56:28 +0600

    Клиенты для системы Identity

--- Services/SargeStore.Interfaces/Services/IRoleClient.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-using SargeStoreDomain.Entities.Identity;
-
-namespace SargeStore.Interfaces.Services
-{
-    public interface IRoleClient : IRoleStore<Role> { }
-}
