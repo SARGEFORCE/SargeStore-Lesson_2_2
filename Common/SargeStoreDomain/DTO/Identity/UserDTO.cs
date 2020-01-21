87396053a06c240a2cee9e160126bf544782b69f
Sergej Salmin <several989@mail.ru>
2020-01-21 23:45:51 +0600

    DTO-модели системы Identity

---
+++ Common/SargeStoreDomain/DTO/Identity/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace SargeStoreDomain.DTO.Identity
+{
+    public abstract class UserDTO
+    {
+        public UserDTO User { get; set; }
+    }
+
+    public class AddLoginDTO : UserDTO
+    {
+        public UserLoginInfo UserLoginInfo { get; set; }
+    }
+    public class PasswordHashDTO : UserDTO
+    {
+        public string Hash { get; set; }
+    }
+
+    public class SetLockoutDTO : UserDTO
+    {
+        public DateTimeOffset? LockoutEnd { get; set; }
+    }
+}
